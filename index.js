92e9b5a2e8dd052b3fd224888e78f86d9485d4ad
Mimika Ackerman <emily.herreragomez@gmail.com>
2022-12-20 18:24:44 +0100

    server1

---
+++ index.js
@@ -0,0 +1,10 @@
+
+/* console.clear();
+import {createServer} from 'http';
+
+const httpServer = createServer((req,res) =>{
+console.log("PETICION RECIBIDA");
+res.end('the response is successful')
+});
+httpServer.listen(3000) */
+
