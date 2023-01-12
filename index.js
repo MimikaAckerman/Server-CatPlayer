e363e78e0c48811198d81da89f49a7f899d45ff4
Mimika Ackerman <emily.herreragomez@gmail.com>
2023-01-12 14:48:17 +0100

    funciones

--- index.js
+++ index.js
@@ -1,54 +1,70 @@
+import express from "express";
+/* import dotenv from 'dotenv'*/
+import { USERS_DB } from "./database.js";
 
- console.clear();
-import {createServer} from 'http';
+/* dotenv.config();
+process.env.PORT */
+const PORT = 3000;
 
-const httpServer = createServer((req,res) =>{
-console.log("PETICION RECIBIDA");
-res.end('the response is successful')
+const app = express();
+app.use(express.json());
+app.use(express.text());
+
+//--------------------------obtener los detalles de la cuenta
+app.get("/:guid", (req, res) => {
+  const { guid } = req.params;
+  const user = USERS_DB.find((user) => user.guid === req.params.guid);
+
+  if (!user) return res.status(404).send();
+   res.send(user) 
 });
-httpServer.listen(3000) 
-
-/* 
-
-import express  from "express";
-import dotenv from 'dotenv';
-import {USERS_DB} from './database.js'
-
-
-dotenv.config();
-
-
-process.env.PORT;
-/*  const PORT = 3000; */
-/* const expressApp = express();
-
-expressApp.use(express.json());
-expressApp.use(express.text());
-
-//account details
-expressApp.get("/:guid", (req,res)=>{
-
-    const user = USERS_DB.find(user => user.guid === req.params.guid)
-    
-        if(!user) res.status(404).send();
-        res.send(user)
-})
-
-//created account
-expressApp.post()
-
-//update account
-expressApp.patch()
-
-//delete account
-expressApp.delete()
 
 
 
- */
 
-/* 
-expressApp.listen(PORT, () => 
-    console.log(`the server is using the port ${PORT}`)
-);
-  */
+//--------------------crear una nueva cuenta
+ app.post("/", (req, res) => {
+  const { guid, name } = req.body;
+  if (!guid || !name) return res.state(400).send();
+
+  const user = USERS_DB.find((user) => user.guid === guid);
+  if (user) return res.status(409).send();
+
+  USERS_DB.push({
+    guid,
+    name,
+  });
+  return res.send();
+}); 
+
+
+
+//--------------------actualizar la cuenta de una cuenta
+ app.patch("/account/:guid", (req, res) => {
+  const { guid } = req.params;
+  const { name } = req.body;
+
+  if (!name) return res.status(400).send();
+  const user = USERS_DB.find((user) => user.guid === guid);
+
+  if (!user) res.status(404).send();
+  user.name = name;
+  return res.send(user); 
+
+ }); 
+
+//--------------------------------eliminar la cuenta
+ app.delete("/:guid", (req, res) => {
+  const { guid } = req.params;
+  const userIndex = USERS_DB.findIndex((user) => user.guid === guid);
+
+  if (userIndex === -1) return res.status(404).send();
+  USERS_DB.splice(userIndex, 1);
+  return res.send(); 
+  
+ }); 
+
+
+
+
+app.listen(PORT, () => console.log(`servidor levantado en el puerto ${PORT}`));
